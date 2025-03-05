9372a638996080cd24f7bf277e8bbd2f6a1da6d9
Yuré Vanderbruggen <vanderbruggen.yure@gmail.com>
2025-03-05 22:04:27 +0100

    feat: updated the install script to use correct bash syntax and user approval

--- install.sh
+++ install.sh
@@ -1,41 +1,62 @@
 #!/bin/bash
 
+echo "This will remove the directories for the configs of i3, i3status, tmux, ghostty and fish."
+echo "Backup your existsing configs in a different place to be safe."
+read -p "Do you want to continue? [y/n]: " yn
+if [ $yn = [Nn]* ]; then
+  exit 1
+fi
+
+user="$(whoami)"
+
 # Check if the ~/.config directory exists
-if [ ! -d "~/.config" ]; then
-  mkdir ~/.config
+config="/home/${user}/.config/"
+if [ ! -d "${config}" ]; then
+  mkdir ${config}
 fi
 
 # i3 configs
-if [ ! -d "~/.config/i3" ]; then
-  mkdir ~/.config/i3
+i3config="/home/${user}/.config/i3/"
+if [ -d "${i3config}" ]; then
+  rm -rf ${i3config}
+  mkdir ${i3config}
+  cp -l ./i3/config ~/.config/i3/config
 fi
-cp -l ./i3/config ~/.config/i3/config
-if [ ! -d "~/.config/i3status" ]; then
-  mkdir ~/.config/i3status
+i3statuconfigs="/home/${user}/.config/i3status/"
+if [ -d "${i3statusconfig}" ]; then
+  rm -rf ${i3statusconfig}
+  mkdir ${i3statusconfig}
+  cp -l ./i3status/config ~/.config/i3status/config
 fi
-cp -l ./i3status/config ~/.config/i3status/config
 
 # Tmux
-cp -l ./tmux/.tmux.conf ~/.tmux.conf
+rm "/home/${user}/.tmux.conf"
+cp -l ./tmux/.tmux.conf "/home/${user}/.tmux.conf"
 
 # Neovim
-if [ ! -d "~/.config/nvim" ]; then
-  mkdir ~/.config/nvim
+nvimconfig="/home/${user}/.config/nvim/"
+if [ -d "${nvimconfig}" ]; then
+  rm -rf ${nvimconfig}
+  mkdir ${nvimconfig}
+  cp -l ./nvim/init.lua ~/.config/nvim/init.lua
 fi
-cp -l ./nvim/init.lua ~/.config/nvim/init.lua
 
 # Ghostty
-if [ ! -d "~/.config/ghostty/" ]; then
-  mkdir ~/.config/ghostty/
+ghosttyconfig="/home/${user}/.config/ghostty/"
+if [ -d "${ghosttyconfig}" ]; then
+  rm -rf ${ghosttyconfig}
+  mkdir ${ghosttyconfig}
+  cp -l ./ghostty/config ~/.config/ghostty/config
+  cp -l ./ghostty/startup.sh ~/.config/ghostty/startup.sh
+  chmod +x ~/.config/ghostty/startup.sh
 fi
-cp -l ./ghostty/config ~/.config/ghostty/config
-cp -l ./ghostty/startup.sh ~/.config/ghostty/startup.sh
-chmod +x ~/.config/ghostty/startup.sh
 
 # Fish
-if [ ! -d "~/.config/fish/" ]; then
-  mkdir ~/.config/fish/
+fishconfig="/home/${user}/.config/fish/"
+if [ -d "${fishconfig}" ]; then
+  rm -rf ${fishconfig}
+  mkdir ${fishconfig}
+  cp -l ./fish/config.fish ~/.config/fish/config.fish
+  cp -l ./fish/fish_prompt.fish ~/.config/fish/fish_prompt.fish
+  cp -l ./fish/fish_variables ~/.config/fish/fish_variables
 fi
-cp -l ./fish/config.fish ~/.config/fish/config.fish
-cp -l ./fish/fish_prompt.fish ~/.config/fish/fish_prompt.fish
-cp -l ./fish/fish_variables ~/.config/fish/fish_variables
